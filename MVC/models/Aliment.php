b185ffa76aa1513ae975fb0cae32aa9247a92ff0
Hachimi <hachimi.bouizegarene@outlook.fr>
2023-03-04 01:01:32 +0100

    ajout du code de la page sondage en mvc (pas fini)

---
+++ MVC/models/Aliment.php
@@ -0,0 +1,81 @@
+<?php
+
+class Aliment{
+    private $nom;
+    private $id;
+    private $categorie;
+    private $proteines;
+    private $glucides;
+    private $lipides;
+    private $sucres;
+    private $fibres;
+    private $fer;
+    private $magnesium;
+    private $manganese;
+    private $calcium;
+    private $chlorure;
+
+    public function __construct($data)
+    {
+        $this->setData($data);
+    }
+
+    public function setData(array $data){
+        foreach($data as $key=>$value){
+            $method = 'set'. ucfirst($key);
+            if(method_exists($this, $method)) $this->$method($value);
+        }
+    }
+
+    //SETTERS
+    public function setNom($nom){
+        $this->nom = $nom;
+    }
+    public function setId($id){
+        $this->id = $id;
+    }
+    public function setCategorie($categorie){
+        $this->categorie = $categorie;
+    }
+    public function setProteines($proteines){
+        $this->proteines = $proteines;
+    }
+    public function setGlucides ($glucides){
+        $this->glucides = $glucides;
+    }
+    public function setLipides ($lipides){
+        $this->lipides = $lipides ;
+    }
+    public function setSucres ($sucres){
+        $this->sucres = $sucres ;
+    }
+    public function setFibres ($fibres){
+        $this->fibres = $fibres ;
+    }
+    public function setFer ($fer){
+        $this->fer = $fer ;
+    }
+    public function setMagnesium ($magnesium){
+        $this->magnesium = $magnesium ;
+    }
+    public function setManganese ($manganese){
+        $this->manganese = $manganese ;
+    }
+    public function setCalcium ($calcium){
+        $this->calcium = $calcium ;
+    }
+    public function setChlorure ($chlorure){
+        $this->chlorure = $chlorure ;
+    }
+
+    //GETERS
+    public function getId(){
+        return $this->id;
+    }
+    public function getNom(){
+        return $this->nom;
+    }
+}
+
+
+?>
---
+++ MVC/views/ViewAccueil.php
@@ -0,0 +1,55 @@
+
+<?php
+//les fonction liees a cette vue
+require_once 'views/functions/accueil.php';
+?>
+
+
+<div class="page">
+        <div class="haut-page">
+            <h1>Resultats sondages</h1>
+            <div class="container">
+                <div class="info-container">
+                    <h2>Nombre de participants:</h2>
+                    <div class="content">
+                        <img src="<?= img('sondage.svg') ?>"alt="Icon personnage"/>
+                        <h2><?= $nbParticipants?></h2>
+                    </div>
+                </div>
+                <div class="info-container">
+                    <h2>Nombre d'aliments appréciés:</h2>
+                    <div class="content">
+                        <img src="<?= img('food.svg') ?>"alt="Icon Aliment"/>
+                        <h2><?= $nbAliments?></h2> 
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="mid-page">
+            <div class="container">
+                <h1>Top 10 des aliments les plus appréciés :</h1>
+                <div class="content">
+                    <?php echo afficher_aliments($top10Aliments) ?> 
+                </div>
+            </div>
+
+            <div class="container">
+                <h1>Consommation de categorie d'aliment par Age :</h1>
+                 <canvas id="graphique1"></canvas>
+            </div>
+
+            <div class="container conso_moy">
+                <h1>Composition moyenne des aliments choisis</h1>
+                <div class="content">
+                    <div class="part">
+                        <h2>Nutriments principaux</h2>
+                        <canvas id="graphique2"></canvas>
+                    </div>
+                    <div class= "part">
+                        <h2>Nutriments secondaires</h2>
+                        <canvas id="graphique3"></canvas>
+                    </div>
+                </div>
+            </div> 
+        </div>
+    </div>
